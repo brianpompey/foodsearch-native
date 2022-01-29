293efd185ca0e29098234e00ddccb2098c1ef75b
brianpompey <brianthescholar@gmail.com>
2022-01-29 14:23:51 -0500

    Adding search bar

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
-//import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
 const navigator = createStackNavigator({
-
+  Search: SearchScreen,
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
 });
+
+
+export default createAppContainer(navigator);
---
+++ src/components/RestaurantList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const RestaurantList = () => {
+    return (
+        <View>
+            <Text>SearchBar</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default RestaurantList;
