efeead2bb7e82c8e31a7a1181582915b01b8df72
brianpompey <brianthescholar@gmail.com>
2022-01-29 21:37:16 -0500

    adding results detail page

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import RestaurantsDetail from "../components/RestaurantsDetail";
+
+const ResultsShowScreen = () => {
+    return (
+        <View>
+            <Text>results</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
