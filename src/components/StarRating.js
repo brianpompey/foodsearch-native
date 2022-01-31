43bd386432e0922f184a7adde57210f2da6d16da
brianpompey <brianthescholar@gmail.com>
2022-01-31 16:11:55 -0500

    Adding a star rating component

---
+++ src/components/StarRating.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const StarRating = () => {
+    return (
+        <View>
+            <Text>Star rating</Text>
+        </View>
+    )
+};
+
+const style = StyleSheet.create({});
+
+export default StarRating;
