56cafaf55de134115f1288da178dd694fb79ce57
brianpompey <brianthescholar@gmail.com>
2022-01-31 16:15:35 -0500

    gonna display it on a new page frist

---
+++ src/screens/StarPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const StarPage = () => {
+    return(
+        <View>
+            <Text>Star StarPage
+            </Text>
+        </View>
+    )
+};
+
+
+const styles = StyleSheet.create({});
+
+export default StarPage;
