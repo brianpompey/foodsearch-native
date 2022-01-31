ba415a333fe4665d4e61b1cdc00eaf37f8f6514a
brianpompey <brianthescholar@gmail.com>
2022-01-31 16:45:01 -0500

    going to try something else

--- src/components/RestaurantsDetail.js
+++ src/components/RestaurantsDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet} from 'react-native';
-import StarRating from '../components/StarRating';
+//import StarRating from '../components/StarRating';
+import { FontAwesome } from '@expo/vector-icons';
 
 
 const RestaurantsDetail = ({ result }) => {
@@ -9,7 +10,17 @@ const RestaurantsDetail = ({ result }) => {
             <Image style={styles.image} source={{ uri: result.image_url }} />
             <Text style={styles.name} >{result.name}</Text>
             <Text>{result.rating} Stars, {result.review_count} Reviews </Text>
-            <StarRating />
+            <View style={styles.starContainer}>
+                {/* <Text>Star rating</Text> */}
+                <View style={styles.innerContainer} >
+                    <FontAwesome name="star" size={24} color="black" />
+                    <FontAwesome name="star" size={24} color="black" />
+                    <FontAwesome name="star" size={24} color="black" />
+                    <FontAwesome name="star-half-full" size={24} color="black" />
+                    <FontAwesome name="star-o" size={24} color="black" />
+                </View>
+            </View>
+            {/* <StarRating /> */}
         </View>
     )
 };
@@ -27,6 +38,17 @@ const styles = StyleSheet.create({
     name: {
         fontWeight: 'bold',
         fontSize: 14,
+    },
+    starContainer: {
+        borderColor: 'black',
+        borderWidth: 1,
+        height: 35,
+        width: 120,
+    },
+    innerContainer: {
+        flexDirection: 'row',
+        marginVertical: 5,
+        marginHorizontal: 4,
     }
 });
 
