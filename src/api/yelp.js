6af956334c1786e14ec2ab77ed222c69ce3b6237
brianpompey <brianthescholar@gmail.com>
2022-02-06 13:05:22 -0500

    Added the one for api key too:

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,6 +5,6 @@ import config from "../../config";
 export default axios.create({
     baseURL: `${config.API_URL}`,
     headers: {
-        Authorization: 'Bearer Em_itxQHmFIJDmJqXUu2LAz5q1JZuOEMEKzYf4vewuldrFyj-AR29zERNKrqhs3f3WTr6YstG_GWd5I7WKglhVmC4dfUoPFJITDKyz9ZY5TT7ipg1VkWoAAoOqv0YXYx'
+        Authorization: `Bearer ${config.API_KEY}`
     }
 });
