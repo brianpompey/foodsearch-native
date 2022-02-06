a13d03e20b61655c0ac94529f0c429c9019e9b1f
brianpompey <brianthescholar@gmail.com>
2022-02-06 13:04:41 -0500

    adding env variables

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+//import env from "react-native-dotenv";
+import config from "../../config";
 
 export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses/',
+    baseURL: `${config.API_URL}`,
     headers: {
         Authorization: 'Bearer Em_itxQHmFIJDmJqXUu2LAz5q1JZuOEMEKzYf4vewuldrFyj-AR29zERNKrqhs3f3WTr6YstG_GWd5I7WKglhVmC4dfUoPFJITDKyz9ZY5TT7ipg1VkWoAAoOqv0YXYx'
     }
